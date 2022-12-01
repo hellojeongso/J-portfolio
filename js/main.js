ff4024b937048768f5071e581e6b531079cea7de
hellojeongso <84715469+hellojeongso@users.noreply.github.com>
2022-12-01 16:56:38 +0900

    commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(function(){
+  // 스크롤시 메뉴색 변경
+  var $header = $('header');
+  $(window).scroll(function(){
+      if($(this).scrollTop() > 600){
+          $header.find('nav ul li a').addClass('menu_black');
+      } else {
+          $header.find('nav ul li a').removeClass('menu_black');
+      }
+  });
+
+  // 슬라이드
+  $('.myslider').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+})
